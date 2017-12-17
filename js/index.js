b0c49a8bb09b909c45869ba832a964a02742d794
Jinhua Wang <jinhuawang@dhcp-10-249-164-133.eduroam.wireless.private.cam.ac.uk>
2017-12-17 04:13:33 +0000

    added view port

--- js/index.js
+++ js/index.js
@@ -1,6 +1,14 @@
+ document.addEventListener('touchmove', function(event) {
+    event = event.originalEvent || event;
+    if(event.scale > 1) {
+      event.preventDefault();
+    }
+  }, false);
+
 $(document).ready(function() {
 		
 			$('#button').click(function(){
 				$('body').toggleClass('day');  
 			});
+
 		});
